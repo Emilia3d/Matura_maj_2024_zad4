7b02426275eb00d91572eaa8d5959789851ad136
Emilia <emilia3d.pulawy@gmail.com>
2026-01-27 23:37:23 +0100

    4.3

--- Matura_maj_2024_zad4/Matura_maj_2024_zad4.cpp
+++ Matura_maj_2024_zad4/Matura_maj_2024_zad4.cpp
@@ -13,6 +13,18 @@ using namespace std;
 //const int N1 = 200;
 //const int N2 = 20;
 
+ vector<int> rozkladNaPierwsze(int liczba) {
+     vector<int> czynniki;
+     for (int i = 2; i <= liczba; i++) {
+         while (liczba % i == 0) {
+             czynniki.push_back(i);
+             liczba /= i;
+         }
+         if (liczba == 1) break;
+     }
+     return czynniki;
+ }
+
 int main() {
     vector<int> liczby1;
     vector<long long> liczby2;
@@ -33,35 +45,26 @@ int main() {
 
     cout << 4.1 << endl;
 
-    int ile_dzielnikow = 0;
+    int licznik = 0;
 
-    for (int i = 0; i < (int)liczby1.size(); i++) {
-        int a = liczby1[i];
-        bool jestDzielnikiem = false;
-
-        for (int j = 0; j < (int)liczby2.size(); j++) {
-            long long b = liczby2[j];
-            if (b % a == 0) {
-                jestDzielnikiem = true;
-                break;
+    // Sprawdzanie podzielności
+    for (int liczba1 : liczby1) {
+        for (int liczba2 : liczby2) {
+            if (liczba2 % liczba1 == 0) {
+                licznik++;
+                break; // Przerwij wewnętrzną pętlę, gdy znajdziesz podzielną liczbę
             }
         }
-
-        if (jestDzielnikiem) ile_dzielnikow++;
     }
+    cout << licznik << endl;
 
-
+    cout << "4.2 " << endl;
     vector<int> kopia = liczby1;
     sort(kopia.rbegin(), kopia.rend());
     
+    int najw101 = kopia[101]; 
+ 
+    cout << najw101 << endl;
 
-    int najw101 = kopia[101]; // zakładamy, że N1 >= 2
-
-    cout << "4.1 " << ile_dzielnikow << endl;
-    cout << "4.2 " << najw101 << endl;
-
-
-    cout << "Zapisano odpowiedzi do wyniki4.txt" << endl;
-
-    return 0;
+   return 0;
 }
